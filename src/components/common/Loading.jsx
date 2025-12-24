89b7cd94b2943143c8924945ac806349b79644c5
sssannishah-stack <ss.sannishah@gmail.com>
2025-12-24 20:20:30 +0530

    Update Loading.jsx

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,1 +1,23 @@
-import React from 'react'; export default () => <div>Loading</div>;
+import React from 'react';
+import { useLanguage } from '../../contexts/LanguageContext';
+import './Loading.css';
+
+export default function Loading({ fullScreen = true, text }) {
+  const { t } = useLanguage();
+
+  const content = (
+    <div className={`loading-container ${fullScreen ? 'fullscreen' : ''}`}>
+      <div className="loading-content">
+        <div className="loading-spinner"></div>
+        <p className="loading-text">{text || t('loading')}</p>
+        <div className="loading-dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+
+  return content;
+}
